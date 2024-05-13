432345181ff563875eb4222af48a92a220346804
appop10 <appoplawski10@gmail.com>
2024-05-13 16:52:01 -0500

    buyWeapons function works (somewhat)

--- rolePlayingGame/script.js
+++ rolePlayingGame/script.js
@@ -104,7 +104,21 @@ function buyHealth() {
 }
 
 function buyWeapon() {
+    if (currentWeapon < weapons.length) {
+        if (gold >= 30) {
+            gold -= 30;
+            currentWeapon++;
     
+            goldText.innerText = gold;
+            let newWeapon = weapons[currentWeapon].name;
+            text.innerText = "You now have a " + newWeapon + ".";
+            text.innerText += " In your inventory you have: " + inventory.join(", ") + ".";
+    
+            inventory.push(newWeapon);
+        } else {
+            text.innerText = "You do not have enough gold to buy a weapon.";
+        }
+    }
 }
 
 function fightSlime() {
