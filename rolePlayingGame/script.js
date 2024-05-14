93684a4858881286e89d3829fc4d27f3eefa3cc2
appop10 <appoplawski10@gmail.com>
2024-05-14 12:43:21 -0500

    lose and restart functions added new locations for those functions also added

--- rolePlayingGame/script.js
+++ rolePlayingGame/script.js
@@ -85,6 +85,12 @@ const locations = [
         "button text": ["Go to town square", "Go to town square", "Go to town square"],
         "button functions": [goTown, goTown, goTown],
         text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+    },
+    {
+        name: "lose",
+        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+        "button functions": [restart, restart, restart],
+        text: "You die. &#x2620;"
     }
 ];
 
@@ -221,4 +227,22 @@ function defeatMonster() {
     update(locations[4]);
 }
 
-function lose() {}
+function lose() {
+    update(locations[5]);
+}
+
+function restart() {
+    xp = 0;
+    health = 100;
+    gold = 50;
+    currentWeapon = 0;
+    fighting;
+    monsterHealth;
+    inventory = ["stick"];
+
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+    healthText.innerText = health;
+
+    goTown();
+}
