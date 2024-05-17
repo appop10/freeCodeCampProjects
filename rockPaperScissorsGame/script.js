2d0d2422b333e907d81a1a3d9757529b56f06d98
appop10 <appoplawski10@gmail.com>
2024-05-17 13:09:31 -0500

    create showResults()

--- rockPaperScissorsGame/script.js
+++ rockPaperScissorsGame/script.js
@@ -1,6 +1,3 @@
-let playerScore = 0;
-let computerScore = 0;
-
 function getRandomComputerResult() {
     const options = ["Rock", "Paper", "Scissors"];
     const randomIndex = Math.floor(Math.random() * options.length);
@@ -24,17 +21,40 @@ function hasPlayerWonTheRound(player, computer) {
     return playerWins;
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 function getRoundResults(userOption) {
     const computerResult = getRandomComputerResult();
-    const playerWins = hasPlayerWonTheRound(userOption, computerResult);
    
-    if (userOption === computerResult) {
-        return `It's a tie! Both chose ${userOption}`;
-    } else if (playerWins) {
+    if (hasPlayerWonTheRound(userOption, computerResult)) {
         playerScore++;
         return `Player wins! ${userOption} beats ${computerResult}`;
-    } else {
+      } else if (computerResult === userOption) {
+        return `It's a tie! Both chose ${userOption}`;
+      } else {
         computerScore++;
         return `Computer wins! ${computerResult} beats ${userOption}`;
-    }
+      }
 }
+
+const playerScoreSpanElement = document.getElementById("player-score");
+const computerScoreSpanElement = document.getElementById("computer-score");
+const roundResultsMsg = document.getElementById("results-msg");
+const winnerMsgElement = document.getElementById("winner-msg");
+const optionsContainer = document.querySelector(".options-container");
+const resetGameBtn = document.getElementById("reset-game-btn");
+
+function showResults(userOption) {
+    roundResultsMsg.innerText = getRoundResults(userOption);
+    playerScoreSpanElement.innerText = playerScore;
+    computerScoreSpanElement.innerText = computerScore;
+
+    if (playerScore === 3 || computerScore === 3) {
+        winnerMsgElement.innerText = `${
+            playerScore === 3 ? "Player" : "Computer"
+          } has won the game!`;
+        optionsContainer.style.display = "none";
+        resetGameBtn.style.display = "block";
+    } 
+};
