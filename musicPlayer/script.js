2eb381b423c35daf322599cc2fd071b0f3851963
appop10 <appoplawski10@gmail.com>
2024-05-21 11:52:19 -0500

    add click event to shuffle button

--- musicPlayer/script.js
+++ musicPlayer/script.js
@@ -223,3 +223,4 @@ playButton.addEventListener("click", () => {
 pauseButton.addEventListener("click", pauseSong);
 nextButton.addEventListener("click", playNextSong);
 previousButton.addEventListener("click", playPreviousSong);
+shuffleButton.addEventListener("click", shuffle);
