72ce6b2c3f9b2f4e8e583a0b59deddd4d4756b43
appop10 <appoplawski10@gmail.com>
2024-05-23 15:27:29 -0500

    add event listener to the button create a function to check if input is palindrome

--- palindromeChecker/script.js
+++ palindromeChecker/script.js
@@ -4,8 +4,24 @@ window.addEventListener('DOMContentLoaded', () => {
     const palindromeResultEl = document.querySelector('#result');
 
     palindromeButton.addEventListener('click', () => {
-        if (palindromeInput.value === '') {
+        const inputValue = palindromeInput.value;
+
+        if (inputValue === '') {
             alert('Please input a value');
+        } else {
+            const isPalindrome = checkPalindrome(inputValue);
+
+            if (isPalindrome) {
+                palindromeResultEl.innerText = `${inputValue} is a palindrome`;
+            } else {
+                palindromeResultEl.innerText = `${inputValue} is not a palindrome`;
+            }
         }
     });
 });
+
+function checkPalindrome(input) {
+    const reversedStr = input.split('').reverse().join('');
+
+    return input === reversedStr ? true : false;
+}
