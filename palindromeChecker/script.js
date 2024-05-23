7fe0bca86fa810e9650890a5de37f0990b740954
appop10 <appoplawski10@gmail.com>
2024-05-23 15:24:14 -0500

    delete p element inside result div

--- palindromeChecker/script.js
+++ palindromeChecker/script.js
@@ -1,3 +1,11 @@
-const palindromeInput = document.querySelector('#text-input');
-const palindromeButton = document.querySelector('#check-btn');
-const palindromeResultEl = document.querySelector('#result');
+window.addEventListener('DOMContentLoaded', () => {
+    const palindromeInput = document.querySelector('#text-input');
+    const palindromeButton = document.querySelector('#check-btn');
+    const palindromeResultEl = document.querySelector('#result');
+
+    palindromeButton.addEventListener('click', () => {
+        if (palindromeInput.value === '') {
+            alert('Please input a value');
+        }
+    });
+});
