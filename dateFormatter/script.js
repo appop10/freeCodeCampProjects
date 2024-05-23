23303a96a2b4f72bb1a6f2e5a103a8b27d30cf36
appop10 <appoplawski10@gmail.com>
2024-05-23 14:17:18 -0500

    display formatted date to the page

--- dateFormatter/script.js
+++ dateFormatter/script.js
@@ -9,3 +9,7 @@ const hours = date.getHours();
 const minutes = date.getMinutes();
 
 const formattedDate = `${day}-${month}-${year}`;
+
+currentDateParagraph.textContent = formattedDate;
+
+dateOptionsSelectElement.addEventListener('change', () => {});
