54c3d12b7dfbae89917e98417e7c23942839ee72
appop10 <appoplawski10@gmail.com>
2024-05-23 15:15:52 -0500

    create variables to hold input, button, and output elements

---
+++ palindromeChecker/script.js
@@ -0,0 +1,3 @@
+const palindromeInput = document.querySelector('#text-input');
+const palindromeButton = document.querySelector('#check-btn');
+const palindromeResultEl = document.querySelector('#result');
