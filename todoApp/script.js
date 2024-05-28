dba76e7449bdbd33455acab19c89a4c7d3ce1ecf
appop10 <appoplawski10@gmail.com>
2024-05-28 15:41:11 -0500

    use reset() in the dicardBtn and taskForm submit

--- todoApp/script.js
+++ todoApp/script.js
@@ -35,7 +35,7 @@ cancelBtn.addEventListener("click", () => {
 
 discardBtn.addEventListener("click", () => {
     confirmCloseDialog.close();
-    taskForm.classList.toggle("hidden");
+    reset();
 });
 
 taskForm.addEventListener("submit", (e) => {
@@ -67,5 +67,5 @@ taskForm.addEventListener("submit", (e) => {
         `;
     });
 
-    taskForm.classList.toggle("hidden");
+    reset();
 });
